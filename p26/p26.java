0c4d175994fff00e1d7c4809db878b82ec71fcee
wenjunlo <lucy921231@gmail.com>
2024-11-26 23:08:20 +0800

    first commit

---
+++ p26/p26.java
@@ -0,0 +1,36 @@
+package p26;
+
+public class p26 {
+	public static void main(String[] args) {
+		Car[] cars;
+		cars=new Car[2];
+		
+		cars[0]=new Car();
+		cars[1]=new RacingCar();
+		
+		for(int i=0;i<cars.length;i++) {
+			Class cl=cars[i].getClass();
+			System.out.println("第"+(i+1)+"個物件類別是"+cl);
+		}
+		
+	}
+
+}
+
+
+class Car{
+	protected int num;
+	protected double gas;
+	
+	public Car() {
+		num=0;
+		gas=0;
+		System.out.println("生產了車子");
+	}
+}
+
+class RacingCar extends Car{	
+	public RacingCar(){
+		System.out.println("生產了賽車");
+	}
+}
